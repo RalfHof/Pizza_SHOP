6646c69c91b4020583ec03090675383bee95be20
RalfHof <email adresse>
2024-11-28 09:58:23 +0100

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import pool from "./db.js";
+import karteRoutes from "./routes/karte.js"
+import zutatenRoutes from "./routes/zutaten.js"
+import bestellungenRoutes from "./routes/bestellungen.js"
+
+const app = express()
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json())
+
+
+app.use("/api/karte", karteRoutes)
+app.use("/api/zutaten", zutatenRoutes)
+app.use("/api/bestellungen", bestellungenRoutes)
+
+
+app.listen(PORT, () => {
+    console.log(`Server läuft auf http://localhost:${PORT}`);
+});
